97e53db3816b32535406846db7e5ee820a21b194
Sam <super8989@gmail.com>
2019-09-03 22:50:57 -0400

    typing test application completed

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const wording = ["Do you like JavaScript as much as I do?", "Hope you are having fun this is a simple game", "This is a typing test. Go ahead and type along"];
+const wording = ["Do you like JavaScript as much as I do?", "Hope you are having fun this is a simple game", "This is a typing test. Go ahead and type along", "New typing test challenge"];
 
 let startTime, endTime;
 
@@ -6,8 +6,8 @@ const message = document.querySelector(".message");
 const playText = document.querySelector("textarea");
 const button = document.querySelector("button");
 
-button.addEventListener("click", function() {
-    console.log(this.innerText);
+button.addEventListener("click", function () {
+ 
     if (this.innerText == "Start") {
         playText.disabled = false;
         playGame();
@@ -19,37 +19,34 @@ button.addEventListener("click", function() {
 })
 
 function playGame() {
-    let randomNum = Math.floor(Math.random()*wording.length);
-    message.innerText = wording[randomNum];
-    let date = new Date();
-    startTime = date.getTime();
+    let randomNum = Math.floor(Math.random() * wording.length);
 
-    console.log(startTime);
+    message.innerText = wording[randomNum];
+
+    let date = new Date();
+
+    startTime = date.getTime();
 
     button.innerText = "Done";
 
-    console.log(randomNum);
-} 
+}
 
 
 function endPlay() {
     let date = new Date();
     endTime = date.getTime();
-    let totalTime = ((endTime-startTime)/1000);
-
-    console.log(totalTime);
+    let totalTime = ((endTime - startTime) / 1000);
 
     let str = playText.value;
-
     let wordCount = wordCounter(str);
-
-    let speed = Math.round((wordCount/totalTime)*60);
-
-    console.log(speed);
-
+    let speed = Math.round((wordCount / totalTime) * 60);
     let finalMessage = "You typed at " + speed + " words per minute.";
-    message.innerHTML = finalMessage;
 
+    if (str != message.innerText) {
+        finalMessage += "<br>There were some errors in the wording";
+    }
+
+        message.innerHTML = finalMessage;
 
     //count words
     //divide by total time words per minute
@@ -58,6 +55,5 @@ function endPlay() {
 
 function wordCounter(strWords) {
     let response = strWords.split(" ").length;
-    console.log(response);
     return response;
 }
