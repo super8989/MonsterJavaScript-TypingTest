3ef25f486a8c36105de3ab78da02fc9cda1c60b0
Sam <super8989@gmail.com>
2019-09-03 22:30:21 -0400

    adding in the typing timer

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const wording = ["Do you like JavaScript as much as I do?", "Hope you are having fun this is a simple game", "This is a typing test. Go ahead and type along"];
 
+let startTime, endTime;
+
 const message = document.querySelector(".message");
 const playText = document.querySelector("textarea");
 const button = document.querySelector("button");
@@ -15,5 +17,12 @@ button.addEventListener("click", function() {
 function playGame() {
     let randomNum = Math.floor(Math.random()*wording.length);
     message.innerText = wording[randomNum];
+    let date = new Date();
+    startTime = date.getTime();
+
+    console.log(startTime);
+
+    button.innerText = "Done";
+
     console.log(randomNum);
 } 
